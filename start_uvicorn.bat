1f63e6f6fea6210f581ac5e91d4cdb2d09ba1a81
hmzza <hamza.cstn@gmail.com>
2025-07-09 11:21:06 +0500

    Remove ignored files from Git tracking

--- start_uvicorn.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd C:\Users\Administrator\Desktop\JBot_Backend
-C:\Users\Administrator\AppData\Local\Programs\Python\Python311\python.exe -m uvicorn main:app --host 0.0.0.0 --port 9000 --log-level info
